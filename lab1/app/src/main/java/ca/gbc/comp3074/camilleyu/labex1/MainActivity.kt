6a356e7fd288e75c165feca3552b826a3eb96585
kidcat8288 <kamea8288@icloud.com>
2026-09-24 21:24:59 -0400

    finish all the layout

--- lab1/app/src/main/java/ca/gbc/comp3074/camilleyu/labex1/MainActivity.kt
+++ lab1/app/src/main/java/ca/gbc/comp3074/camilleyu/labex1/MainActivity.kt
@@ -14,8 +14,8 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        val message = findViewById<TextView>(R.id.textViewMessage)
-        val button = findViewById<Button>(R.id.buttonChangeText)
+        val message = findViewById<TextView>(R.id.countOutput)
+        val button = findViewById<Button>(R.id.subtractionButton)
 
         button.setOnClickListener {
             message.text = "Button clicked!"
